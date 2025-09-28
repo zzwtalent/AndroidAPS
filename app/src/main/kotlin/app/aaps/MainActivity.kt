4fc39dd4c17878c9c5a3dc9fa1a12c2f99c4505c
zzwtalent <zzwpower@163.com>
2025-09-28 14:13:36 +0800

    fix

--- app/src/main/kotlin/app/aaps/MainActivity.kt
+++ app/src/main/kotlin/app/aaps/MainActivity.kt
@@ -403,15 +403,17 @@ class MainActivity : DaggerAppCompatActivityWithResult() {
 
         // Tabs
         if (preferences.get(BooleanKey.OverviewShortTabTitles)) {
-            binding.tabsNormal.visibility = View.GONE
-            binding.tabsCompact.visibility = View.VISIBLE
+            binding.tabsNormal?.visibility = View.GONE
+            binding.tabsCompact?.visibility = View.VISIBLE
             binding.toolbar.layoutParams = LinearLayout.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT, resources.getDimension(app.aaps.core.ui.R.dimen.compact_height).toInt())
-            TabLayoutMediator(binding.tabsCompact, binding.mainPager) { tab, position ->
-                tab.text = (binding.mainPager.adapter as TabPageAdapter).getPluginAt(position).nameShort
-            }.attach()
+            binding.tabsCompact?.let {\ tabs ->
+                TabLayoutMediator(tabs, binding.mainPager) { tab, position ->
+                    tab.text = (binding.mainPager.adapter as TabPageAdapter).getPluginAt(position).nameShort
+                }.attach()
+            }
         } else {
-            binding.tabsNormal.visibility = View.VISIBLE
-            binding.tabsCompact.visibility = View.GONE
+            binding.tabsNormal?.visibility = View.VISIBLE
+            binding.tabsCompact?.visibility = View.GONE
             val typedValue = TypedValue()
             if (theme.resolveAttribute(android.R.attr.actionBarSize, typedValue, true)) {
                 binding.toolbar.layoutParams = LinearLayout.LayoutParams(
@@ -419,9 +421,11 @@ class MainActivity : DaggerAppCompatActivityWithResult() {
                     TypedValue.complexToDimensionPixelSize(typedValue.data, resources.displayMetrics)
                 )
             }
-            TabLayoutMediator(binding.tabsNormal, binding.mainPager) { tab, position ->
-                tab.text = (binding.mainPager.adapter as TabPageAdapter).getPluginAt(position).name
-            }.attach()
+            binding.tabsNormal?.let {\ tabs ->
+                TabLayoutMediator(tabs, binding.mainPager) { tab, position ->
+                    tab.text = (binding.mainPager.adapter as TabPageAdapter).getPluginAt(position).name
+                }.attach()
+            }
         }
     }
 
